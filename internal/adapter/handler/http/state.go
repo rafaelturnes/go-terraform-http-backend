9b2fa0d19dd2b8b4004956d8fc73618713fc0876
Rafael Turnes <rafael@turnes.com.br>
2024-05-23 23:16:14 -0300

    chore: dump request

--- internal/adapter/handler/http/state.go
+++ internal/adapter/handler/http/state.go
@@ -1,19 +1,42 @@
 package http
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/gin-gonic/gin"
 )
 
 func fetch(c *gin.Context) {
+	reqDump, err := httputil.DumpRequest(c.Request, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("REQUEST:\n%s", string(reqDump))
+
 	c.String(http.StatusOK, "get state", nil)
 }
 
 func update(c *gin.Context) {
+	reqDump, err := httputil.DumpRequest(c.Request, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("REQUEST:\n%s", string(reqDump))
+
 	c.String(http.StatusOK, "update state", nil)
 }
 
 func purge(c *gin.Context) {
+	reqDump, err := httputil.DumpRequest(c.Request, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("REQUEST:\n%s", string(reqDump))
+
 	c.String(http.StatusOK, "delete state", nil)
 }
